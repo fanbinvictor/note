1fa9c71ba9d8c392e17558fc941bbb2482eed0d1
fanbin <732155316@qq.com>
2018-09-04 17:02:49 +0800

    样式与显示修改

--- config/administrator.php
+++ config/administrator.php
@@ -61,6 +61,7 @@ return array(
      */
     'permission' => function () {
         // 只要是能管理内容的用户，就允许访问后台
+        return true;
         return Auth::check() && Auth::user()->can('manage_contents');
     },
 
