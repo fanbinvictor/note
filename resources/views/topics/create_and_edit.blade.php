a73c422c30c759ed2e58a0f9717601110eb1583c
fanbin <732155316@qq.com>
2018-09-13 21:19:48 +0800

    编辑器修改

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -62,40 +62,14 @@
 @stop
 
 @section('scripts')
-<!--    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>-->
-<!--    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>-->
-<!--    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>-->
-<!--    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>-->
     <script type="text/javascript"  src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
-
     <script>
     $(document).ready(function(){
-       /* var editor = new Simditor({
-            textarea: $('#editor'),
-            upload: {
-                url: '{{ route('topics.upload_image') }}',
-                params: { _token: '{{ csrf_token() }}' },
-                fileKey: 'upload_file',
-                connectionCount: 3,
-                leaveConfirm: '文件上传中，关闭此页面将取消上传。'
-            },
-            pasteImage: true,
-        });*/
         CKEDITOR.replace('editor', {
             language : 'zh-cn',
-            isFileUploadSupported : true,
+            isFileUploadSupported : false,
             filebrowserUploadUrl: '{{ route('topics.upload_image') }}?_token={{csrf_token()}}'
     });
-        //CK5
-//        ClassicEditor
-//            .create( document.querySelector( '#editor' ) )
-//            .then( editor => {
-//            console.log( editor );
-//    } )
-//    .catch( error => {
-//            console.error( error );
-//    } );
     });
     </script>
-
 @stop
